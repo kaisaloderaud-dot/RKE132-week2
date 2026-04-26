4acb260fb94f7f2052d7d89cca0cec907c46a1e1
kaisaloderaud-dot <kaisa.loderaud@tktk.ee>
2026-04-26 20:10:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+""" day = input ("Mis päev on homme? (tööpäev/puhkepäev):")
+if day == "tööpäev":
+    print("Lähen magama, head ööd!")
+elif day == "puhkepäev":
+    print("Veel üks osa netflixist")
+else:
+    print("vale väärtus")
+ """
+
+""" print("Tere tulemast programmi 'Finantsnõustaja'!")
+print("Sinu isiklik nõustaja ei tee emotsionaalseid oste.")
+money = int(input ("Kui palju raha on sul praegu "))
+if money < 2500:
+    print("Sul pole veel piisavalt raha. Ole kannatlik ja kogu edasi")
+elif money == 2500:
+    print("Piisavalt raha")
+else:
+    print("Saad osta iphone ja veel jääb raha üle") """
+ 
+goal = 1000
+steps = int(input("Mitu sammu oled juba teinud:"))
+
+percent = (steps/goal) * 100
+
+if percent < 50:
+    print("Alles poolel teel, liigu edasi")
+elif percent < 75:
+    print("Tubli, oled peaaegu kohal")
+elif print < 100:
+    print("Suurepärane, oled peaaegu kohal")
+else:
+    print("Palju õnne, oled eesmärgi täitnud") 
