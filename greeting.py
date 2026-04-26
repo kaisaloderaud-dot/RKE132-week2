1555810783374213bb9a7a2b7fcede75ed2d5073
kaisaloderaud-dot <kaisa.loderaud@tktk.ee>
2026-04-26 20:26:09 +0300

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,10 @@
+perekonnanimi = input("Sisestage oma perekonnanimi: ")
+sugu = input("Valihe sugu (m/n): ")
+
+if sugu == "m":
+    print("Tere, härra (perekonnanimi)!")
+elif sugu == "n":
+    print("Tere, proua (perekonnanimi)!")
+else:
+    print("Tere tulemast, (perekonnanimi)!")
+    
